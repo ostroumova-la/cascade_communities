679148a37531ec0d84c7c00f9545d9159c956c6d
Liudmila Prokhorenkova <ostroumova-la@sandbox-353943708>
2020-02-29 15:54:36 +0300

    improved readability

---
+++ base_algorithms_twitter_fast.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import print_function
+
+import partition_metrics
+import networkx as nx
+import community_ext
+import sys, os
+from collections import defaultdict
+import numpy as np
+from math import pow, exp
+import subprocess
+import time as tm
+
+def cos_sim(a, b, norm=True):
+    """Takes 2 vectors a, b and returns the cosine similarity according 
+    to the definition of the dot product
+    """
+    dot_product = np.dot(a, b)
+    if not norm: return dot_product
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return dot_product / (norm_a * norm_b)
+
+def cos_sim_set(a_set, b_set, norm=True):
+    """Takes 2 sets of non-zero bits indexes from vectors a, b and returns the cosine similarity according 
+    to the definition of the dot product
+    """
+    dot_product = len(a_set&b_set)
+    if not norm: return dot_product
+    norm_a = np.sqrt(len(a_set))
+    norm_b = np.sqrt(len(b_set))
+    return dot_product / (norm_a * norm_b)
+
+
+def get_clusters(fn):
+    groundtruth_partition = dict()
+    i = 0
+    for line in open(fn):
+        node, cluster = map(int, line.rstrip().split("\t"))
+        groundtruth_partition[node] = cluster
+    return groundtruth_partition
+
+# get ground truth graph and partition
+groundtruth_partition = get_clusters("graph/graph.clusters")
+
+algorithm = sys.argv[1] # oracle, multitree, path, wpath, clique, read
+filename = algorithm
+
+if algorithm == "clique":
+    lmd0 = float(sys.argv[2])
+    lmd = lmd0
+    filename += "_"+str(lmd)
+    
+dir = os.path.dirname("results/")
+if not os.path.exists(dir):
+    os.makedirs(dir)
+    
+res_file = open("results/"+filename, "w")
+res_time = open("results/"+filename+"_time", "w")
+metrics = ['sub_jaccard','sub_nmi','sub_nmi_arithm','sub_fnmi','sub_fnmi_arithm','sub_F-measure','sub_pearson','jaccard_diff','nmi_fixed','nmi_fixed_arithm','fnmi_fixed','fnmi_fixed_arithm','F-measure_diff','pearson_v2','pearson_v3']
+
+for size in [125, 250, 500, 1000, 2000, 4000, 8000, 16000, 30000]:
+    print(size, end=' ', flush=True)
+    time0 = tm.time()
+    res_file.write(str(size)+"\t")
+    results = dict()
+    flag = False
+
+    ep_fn = "epidemics/"+str(size)
+    known_nodes = set()
+    if algorithm == "read":
+        known_edges = set()
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            for t in z[9:]:
+                zz = t.split("$")
+                node_from = int(zz[0])
+                node_to = int(zz[1])
+                if node_from != -1:
+                    known_edges.add( (str(min(node_from,node_to)),str(max(node_from,node_to))) )
+                known_nodes.add(node_to)
+    if algorithm == "multitree":
+        # create epidemics in multitree format 
+        dst = open("multitree.ep", "w")
+        known_nodes = set()
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            epidemia = []
+            for w in z[9:]:
+                root,node,time = w.split("$")
+                known_nodes.add(int(node))
+        for n in sorted(known_nodes):
+            dst.write(str(n)+","+str(n)+"\n")
+        dst.write("\n")
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            epidemia = []
+            for w in z[9:]:
+                root,node,time = w.split("$")
+                epidemia.append(node)
+                epidemia.append(time)
+            dst.write(",".join(epidemia)+"\n")
+        dst.close()
+
+        # run multitree
+        seen_vertices = str(5*len(known_nodes))
+
+        command = "../network-inference-multitree/network-inference-multitree -i:multitree.ep -e:"+seen_vertices+" -o:mtree > tmp"
+        try:
+            subprocess.check_call(command, shell=True)
+        except:
+            flag = True
+            break
+            
+        # get obtained graph
+        fn = "mtree-edge.info"
+        known_edges = set()
+        for i,l in enumerate(open(fn)):
+            if not i: continue
+            z = l.strip().split('/')
+            node_from = int(z[0])
+            node_to = int(z[1])
+            known_edges.add( (str(min(node_from,node_to)),str(max(node_from,node_to))) )
+    
+    if algorithm == "clique":
+        EPS = 0.000001
+        
+        if lmd0 == -1:
+            sum = 0.
+            num = 0
+            for l in open(ep_fn):
+                prev_time = 0.
+                z = l.strip().split('\t')
+                for t in z[10:]:
+                    zz = t.split("$")
+                    cur_time = float(zz[2])
+                    num+=1
+                    sum+=(cur_time-prev_time)
+                    prev_time = cur_time
+            lmd = num/sum
+            
+        if lmd0 == -2:
+            sum = 0.
+            num = 0
+            for l in open(ep_fn):
+                z = l.strip().split('\t')
+                for i1 in range(10,len(z)):
+                    zz = z[i1].split("$")
+                    cur_time = float(zz[2])
+                    for i2 in range(9,i1):
+                        zz = z[i2].split("$")
+                        prev_time = float(zz[2])
+                        num+=1
+                        sum+=(cur_time-prev_time)
+            lmd = num/sum
+        
+        projected_cascade_sequence_lmd = defaultdict(float)
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            if int(z[3]) == 0: continue
+            seq = []
+            for t in z[9:]:
+                zz = t.split("$")
+                time = float(zz[2])
+                node_to = zz[1]
+                seq.append([time,node_to])
+                known_nodes.add(int(node_to))
+            seq = sorted(seq,key=lambda x:x[0])
+
+            norm_count = 0.
+            t2_old = 0.
+            for i2 in range(1,len(seq)):
+                t2 = seq[i2][0]
+                norm_count = (norm_count + 1.) * exp( -lmd * (t2-t2_old))
+                t2_old = t2
+
+                for i1 in range(0,i2):
+                    t1 = seq[i1][0]
+                    key = min(seq[i1][1],seq[i2][1])+"-"+max(seq[i1][1],seq[i2][1])
+                    projected_cascade_sequence_lmd[key] += exp( -lmd * (t2-t1) ) / norm_count
+                    
+        known_edges = {}
+        for t in projected_cascade_sequence_lmd.items():
+            if t[1]<EPS: continue
+            nn1,nn2 = t[0].split("-")
+            known_edges[(nn1,nn2)] = t[1]
+       
+    if algorithm == "oracle":               
+        known_edges = set()
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            if int(z[3]) == 0:
+                print("error!")
+                exit(0)
+
+            for t in z[9:]:
+                zz = t.split("$")
+                node_from = int(zz[0])
+                node_to = int(zz[1])
+                if node_from != -1:
+                    known_edges.add( (str(min(node_from,node_to)),str(max(node_from,node_to))) )
+                known_nodes.add(node_to)
+                
+    if algorithm == "path":
+        known_edges = set()
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            if int(z[3]) == 0: continue
+
+            prev = -1
+            for t in z[9:]:
+                zz = t.split("$")
+                node_from = prev
+                node_to = int(zz[1])
+                if node_from != -1:
+                    known_edges.add( (str(min(node_from,node_to)),str(max(node_from,node_to))) )
+                prev = node_to
+                known_nodes.add(node_to)
+                
+    if algorithm == "ramezani":
+        EPS = 0.000001
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            if int(z[3]) == 0: continue
+            for t in z[9:]:
+                zz = t.split("$")
+                node_to = int(zz[1])
+                known_nodes.add(node_to)
+
+        nodes_signatures = defaultdict(set)
+        cur_epidemic = 0
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            if int(z[3]) == 0: continue
+            ep_nodes = set()
+            for t in z[9:]:
+                zz = t.split("$")
+                node_to = int(zz[1])
+                ep_nodes.add(node_to)
+            for n in ep_nodes:
+                nodes_signatures[n].add(cur_epidemic)
+            cur_epidemic += 1
+
+        known_edges = dict()
+        for n1 in known_nodes:
+            for n2 in known_nodes:
+                if int(n2)>int(n1):
+                    value = cos_sim_set(nodes_signatures[n1],nodes_signatures[n2])
+                    if value > EPS:
+                        known_edges[ (str(n1),str(n2)) ] = value
+                
+    if algorithm == "wpath":
+        known_edges = defaultdict(int)
+        for l in open(ep_fn):
+            z = l.strip().split('\t')
+            if int(z[3]) == 0: continue
+
+            prev = -1
+            for t in z[9:]:
+                zz = t.split("$")
+                node_from = prev
+                node_to = int(zz[1])
+                if node_from != -1:
+                    known_edges[ (str(min(node_from,node_to)),str(max(node_from,node_to))) ] += 1
+                prev = node_to
+                known_nodes.add(node_to)
+                
+    # quit if read only
+    if algorithm != "read":
+       
+        # create our graph
+        G = nx.Graph()
+        for v in known_nodes:
+            G.add_node(v)
+        for e in known_edges:
+            if algorithm in ("oracle","multitree","path"):
+                from_node, to_node = int(e[0]), int(e[1])
+                G.add_edge(from_node,to_node)
+                #G.add_edge(from_node,to_node,**{"weight": 1})                
+            if algorithm in ("wpath","clique","ramezani"):
+                from_node, to_node = int(e[0]), int(e[1])
+                wght = float(known_edges[e])
+                G.add_edge(from_node,to_node,**{"weight": wght})
+
+        partition = community_ext.best_partition(G, model="dcppm", pars={'gamma':1.0}, randomize=True, weight="weight")
+    
+        assert(set(partition).issubset(groundtruth_partition))
+        assert(known_nodes==set(partition))
+    
+        scores = partition_metrics.compare_partitions_metrics(partition,groundtruth_partition)
+
+        for metric in metrics:
+            results[metric] = scores[metric]
+    
+    if algorithm == "read":
+       time1 = tm.time()
+       res_time.write(str(size)+"\t")
+       res_time.write(str(time1-time0)+"\n")
+       res_time.flush()
+       res_file.write("\n")
+       continue
+    
+    
+    if flag:
+        print('iteration failed')
+        res_file.write("\n")
+        res_file.flush()
+        res_time.write(str(size)+"\n")
+        res_time.flush()
+        continue
+
+    for metric in metrics:
+        res_file.write(str(results[metric])+"\t")
+
+    res_file.write("\n")
+    res_file.flush()
+    time1 = tm.time()
+    res_time.write(str(size)+"\t")
+    res_time.write(str(time1-time0)+"\n")
+    res_time.flush()
+print()
---
+++ opt_algorithms.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import print_function
+
+import partition_metrics
+import networkx as nx
+import community_ext
+import sys, os
+from collections import defaultdict
+import numpy as np
+from math import pow, exp
+import subprocess
+import time as tm
+
+def get_graph(fn):
+    G = nx.Graph()
+    for line in open(fn):
+        from_node, to_node = map(int, line.rstrip().split("\t"))
+        if from_node != to_node:
+            if from_node not in G or to_node not in G[from_node]:
+                G.add_edge(from_node,to_node)
+
+    #get ground truth partition
+    fn1 = fn.replace(".edges",".clusters")
+    groundtruth_partition = dict()
+    
+    for line in open(fn1):
+        node, cluster = map(int, line.rstrip().split("\t"))
+        if node not in G.nodes(): continue
+        groundtruth_partition[node] = cluster
+    return G, groundtruth_partition
+
+# get ground truth graph and partition
+G0, groundtruth_partition = get_graph("graph/graph.edges")
+
+epidemics = sys.argv[1] # type of epidemics
+algorithm = sys.argv[2] # GraphOpt, ClustOpt
+filename = epidemics + "_" + algorithm
+iters_beg = int(sys.argv[3])
+iters_end = int(sys.argv[4])
+if algorithm == "ClustOpt":
+    lmd0 = float(sys.argv[5])
+    lmd = lmd0
+    filename += "_"+str(lmd)
+    
+dir = os.path.dirname("results/")
+if not os.path.exists(dir):
+    os.makedirs(dir)
+    
+res_file = open("results/"+filename, "w")
+res_time = open("results/"+filename+"_time", "w")
+metrics = ['sub_jaccard','sub_nmi','sub_nmi_arithm','sub_fnmi','sub_fnmi_arithm','sub_F-measure','sub_pearson','jaccard_diff','nmi_fixed','nmi_fixed_arithm','fnmi_fixed','fnmi_fixed_arithm','F-measure_diff','pearson_v2','pearson_v3']
+
+for size in range(iters_beg,iters_end+1):
+    print(size, end=' ', flush=True)
+    time0 = tm.time()
+    res_file.write(str((pow(2,size)))+"\t")
+    results = defaultdict(lambda: [])
+    
+    for j in range(1,6):
+        ep_fn = epidemics + "-" + str(j) + "/" + str(size)
+        known_nodes = set()
+        if algorithm == "GraphOpt":
+            # run multitree
+            command = "python3 ../stub_GraphOpt.py "+ep_fn
+            subprocess.check_call(command, shell=True)
+
+            partition = dict()
+            for line in open("GraphOpt_results.tsv"):
+                if line.strip():
+                    node, comm = line.strip().split("\t")
+                    partition[int(node)] = int(comm)
+                    known_nodes.add(int(node))
+  
+        if algorithm == "ClustOpt":
+            EPS = 0.000001
+            
+            if lmd0 == -1:
+                sum = 0.
+                num = 0
+                # first variant
+                for l in open(ep_fn):
+                    prev_time = 0.
+                    z = l.strip().split('\t')
+                    for t in z[10:]:
+                        zz = t.split("$")
+                        cur_time = float(zz[2])
+                        num+=1
+                        sum+=(cur_time-prev_time)
+                        prev_time = cur_time
+                if sum:
+                    lmd = num/sum
+                else:
+                    lmd = 0.
+                #print(lmd)
+                
+            if lmd0 == -2:
+                sum = 0.
+                num = 0
+                # first variant
+                for l in open(ep_fn):
+                    z = l.strip().split('\t')
+                    for i1 in range(10,len(z)):
+                        t = z[i1]
+                        zz = t.split("$")
+                        cur_time = float(zz[2])
+                        for i2 in range(9,i1):
+                            t = z[i2]
+                            zz = t.split("$")
+                            prev_time = float(zz[2])
+                            num+=1
+                            sum+=(cur_time-prev_time)
+                if sum:
+                    lmd = num/sum
+                else:
+                    lmd = 0.
+                #print(lmd)
+            
+            projected_cascade_sequence_lmd = defaultdict(float)
+            for l in open(ep_fn):
+                z = l.strip().split('\t')
+                if int(z[3]) == 1: continue
+                seq = []
+                for t in z[9:]:
+                    zz = t.split("$")
+                    seq.append([float(zz[2]),zz[1]])
+                    known_nodes.add(int(zz[1]))
+                seq = sorted(seq,key=lambda x:x[0])
+
+                for i2 in range(1,len(seq)):
+                    t2 = seq[i2][0]
+                    to_update = []
+                    norm_count = 0.
+
+                    for i1 in range(0,i2):
+                        t1 = seq[i1][0]
+                        norm_count += exp( -lmd * (t2-t1) )
+                        key = min(seq[i1][1],seq[i2][1])+"-"+max(seq[i1][1],seq[i2][1])
+                        to_update.append( (key,exp( -lmd * (t2-t1) )) )
+                    for key,count in to_update:
+                        projected_cascade_sequence_lmd[key] += count / norm_count
+                        
+            known_edges = {}
+            for t in projected_cascade_sequence_lmd.items():
+                if t[1]<EPS: continue
+                nn1,nn2 = t[0].split("-")
+                known_edges[(nn1,nn2)] = t[1]
+            # print(known_edges)
+            # create our graph
+            G = nx.Graph()
+            for e in known_edges:
+                from_node, to_node = int(e[0]), int(e[1])
+                wght = float(known_edges[e])
+                G.add_edge(from_node,to_node,**{"weight": wght})
+            # print(G)
+            initial_partition = community_ext.best_partition(G, model="dcppm", pars={'gamma':1.0}, randomize=True, weight="weight")
+            fh = open('ClustOpt_input.tsv','w')
+            for node, cluster in sorted(initial_partition.items()):
+                print('%d\t%d' % (node,cluster),file=fh)
+            fh.close()
+
+            command = "python3 ../stub_ClustOpt_1step.py "+ep_fn+" ClustOpt_input.tsv"
+            subprocess.check_call(command, shell=True)
+
+            partition = dict()
+            for line in open("ClustOpt_results.tsv"):
+                if line.strip():
+                    node, comm = line.strip().split("\t")
+                    partition[int(node)] = int(comm)
+                    known_nodes.add(int(node))
+
+        
+        assert(set(partition).issubset(groundtruth_partition))
+        assert(known_nodes==set(partition))
+
+        scores = partition_metrics.compare_partitions_metrics(partition,groundtruth_partition)
+
+        for metric in metrics:
+            results[metric].append(scores[metric])   
+        
+    for metric in metrics:
+        res_file.write(str(np.mean(results[metric]))+"\t")
+
+    res_file.write("\n")
+    res_file.flush()
+    time1 = tm.time()
+    res_time.write(str(pow(2,size))+"\t")
+    res_time.write(str(time1-time0)+"\n")
+    
+print()
+
